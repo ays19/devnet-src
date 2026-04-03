dfdb2be696ce34aabdf842dbaa26c8702b0dd243
ays19 <sahsanyasir@gmail.com>
2026-04-03 16:15:07 +0600

    modify

--- graphhopper/graphhopper_parse-json_2.py
+++ graphhopper/graphhopper_parse-json_2.py
@@ -1,19 +1,51 @@
 import requests
 import urllib.parse
 
-geocode_url = "https://graphhopper.com/api/1/geocode?"
 route_url = "https://graphhopper.com/api/1/route?"
 loc1 = "Washington, D.C."
 loc2 = "Baltimore, Maryland"
 key = "63c82c8f-f0ce-4edd-95fa-70b5c1133f69"
 
-url = geocode_url + urllib.parse.urlencode({"q":loc1, "limit": "1", "key":key})
+def geocoding (location, key):
+    geocode_url = "https://graphhopper.com/api/1/geocode?"
+    url = geocode_url + urllib.parse.urlencode({"q":location, "limit": "1",
+"key":key})
 
-replydata = requests.get(url)
-json_data = replydata.json()
-json_status = replydata.status_code
+    replydata = requests.get(url)
+    json_data = replydata.json()
+    json_status = replydata.status_code
+    
+    if json_status == 200:
+        json_data = requests.get(url).json()
+        lat=(json_data["hits"][0]["point"]["lat"])
+        lng=(json_data["hits"][0]["point"]["lng"])
+        name = json_data["hits"][0]["name"]
+        value = json_data["hits"][0]["osm_value"]
+        if "country" in json_data["hits"][0]:
+            country = json_data["hits"][0]["country"]
+        else:
+            country=""
+        if "state" in json_data["hits"][0]:
+            state = json_data["hits"][0]["state"]
+        else:
+            state=""
+        if len(state) !=0 and len(country) !=0:
+            new_loc = name + ", " + state + ", " + country
+        elif len(state) !=0:
+            new_loc = name + ", " + country
+        else:
+            new_loc = name
 
-json_status = replydata.status_code
+        print("Geocoding API URL for " + new_loc + " (Location Type: " + value + ")\n"
++ url)
+            
+    else:
+        lat="null"
+        lng="null"
+        new_loc=location
+    return json_status,lat,lng,new_loc
 
-if json_status == 200:
-    print("Geocoding API URL for " + loc1 + ":\n" + url)
+orig = geocoding(loc1, key)
+print(orig)
+dest = geocoding(loc2, key)
+print(dest)
