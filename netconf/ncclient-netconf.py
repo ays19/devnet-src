516f5f2425e94b0e5ef431578afd5a6a6d5a5045
ays19 <sahsanyasir@gmail.com>
2026-04-17 14:19:18 +0600

    Add loopback interface configuration for Cat8kv

--- netconf/ncclient-netconf.py
+++ netconf/ncclient-netconf.py
@@ -1,28 +1,49 @@
 from ncclient import manager
 import xml.dom.minidom
 
-# Connection parameters
+# Connection parameters for the Cisco Cat8kv Sandbox
 m = manager.connect(
     host="10.10.20.48",
     port=830,
     username="developer",
     password="C1sco12345",
     hostkey_verify=False,
-    device_params={'name': 'iosxe'}  # Crucial for Cat8kv/IOS-XE 17.x
+    device_params={'name': 'iosxe'}
 )
 
-# XML with explicit NETCONF namespaces
-netconf_hostname = """
+# Define the loopback configuration XML
+netconf_loopback = """
 <config xmlns:xc="urn:ietf:params:xml:ns:netconf:base:1.0" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
     <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
-        <hostname>CAT8KV-NEW</hostname>
+        <interface>
+            <Loopback>
+                <name>1</name>
+                <description>My first NETCONF loopback</description>
+                <ip>
+                    <address>
+                        <primary>
+                            <address>10.1.1.1</address>
+                            <mask>255.255.255.0</mask>
+                        </primary>
+                    </address>
+                </ip>
+            </Loopback>
+        </interface>
     </native>
 </config>
 """
 
 try:
-    netconf_reply = m.edit_config(target="running", config=netconf_hostname )
-    print("NETCONF Reply for Hostname Change:")
+    # Send the configuration and store the results
+    netconf_reply = m.edit_config(target="running", config=netconf_loopback )
+
+    # Print the prettified XML response
+    print("NETCONF Reply for Loopback Configuration:")
     print(xml.dom.minidom.parseString(netconf_reply.xml).toprettyxml())
+
+except Exception as e:
+    print(f"An error occurred: {e}")
+
 finally:
+    # Gracefully close the session
     m.close_session()
