ac6e17c3bac3d6ade70db1c8500c74092b6c59fc
ays19 <sahsanyasir@gmail.com>
2026-04-17 14:13:40 +0600

    Resolve hostname configuration errors on Cat8kv

--- netconf/ncclient-netconf.py
+++ netconf/ncclient-netconf.py
@@ -1,12 +1,28 @@
 from ncclient import manager
-m = manager.connect(
-    host="192.168.56.101",
-    port=830,
-    username="cisco",
-    password="cisco123!",
-    hostkey_verify=False
-)
-print("#Supported Capabilities (YANG models):")
-for capability in m.server_capabilities:
-    print(capability)
+import xml.dom.minidom
 
+# Connection parameters
+m = manager.connect(
+    host="10.10.20.48",
+    port=830,
+    username="developer",
+    password="C1sco12345",
+    hostkey_verify=False,
+    device_params={'name': 'iosxe'}  # Crucial for Cat8kv/IOS-XE 17.x
+)
+
+# XML with explicit NETCONF namespaces
+netconf_hostname = """
+<config xmlns:xc="urn:ietf:params:xml:ns:netconf:base:1.0" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+    <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+        <hostname>CAT8KV-NEW</hostname>
+    </native>
+</config>
+"""
+
+try:
+    netconf_reply = m.edit_config(target="running", config=netconf_hostname )
+    print("NETCONF Reply for Hostname Change:")
+    print(xml.dom.minidom.parseString(netconf_reply.xml).toprettyxml())
+finally:
+    m.close_session()
