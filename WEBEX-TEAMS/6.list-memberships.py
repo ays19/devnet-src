13dbda7d86bae1abc453e7e797879e9b35a96c05
ays19 <sahsanyasir@gmail.com>
2026-04-19 17:53:53 +0600

    list all members of a specific Webex room

---
+++ WEBEX-TEAMS/6.list-memberships.py
@@ -0,0 +1,11 @@
+import requests
+access_token = 'ZWI1NTBkYzktMzBmYy00ZmJmLTg5Y2UtMTU0ZmM0NmQ2YjkyOTUyYTZkMmItNDhk_P0A1_3053e3a2-13b5-4c1e-8fee-32c0000aec9d'
+room_id = 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS85YzYwZDc4Mi1lNjY3LTQ0ZGItOWM4My03ZGY3ZDdlMGRhYmU'
+url = 'https://webexapis.com/v1/memberships'
+headers = {
+    'Authorization': 'Bearer {}'.format(access_token),
+    'Content-Type': 'application/json'
+}
+params = {'roomId': room_id}
+res = requests.get(url, headers=headers, params=params)
+print(res.json())
